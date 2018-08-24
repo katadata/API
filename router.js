88c2a07549052826ed1a8c0fc972fde3f27584e4
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2018-08-24 16:04:35 +0700

    abis benerin res download

--- router.js
+++ router.js
@@ -1,30 +1,37 @@
 var express = require('express');
 var router = express.Router();
-const json2csv = require('json2csv').parse;
-const fs = require('fs');
+var fs = require('fs');
 var DataFrame = require('dataframe-js').DataFrame;
+var dataForge = require('data-forge');
+
+var ExportToCsv = require('export-to-csv');
+
 
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-    let id = req.params.id;
+router.get('/:nama_data1', function(req, res, next) {
+    var nama_data2 = req.params.nama_data1;
+    var nama_data3 = nama_data2.split("-").join(" ")
+    var nama_data4 = String("'"+nama_data3+"'")
     var query = 'select a.id, b.nama as nama_data, a.data_x as data_x, a.data_y as data_y, ' +
                 'c.nama as nama_produk, d.nama as nama_item, e.nama as nama_negara, ' +
                 'f.nama as nama_provinsi, g.nama as nama_kota, a.satuan as satuan, ' +
                 'a.sumber as sumber from data a left join nama_data b ' +
                 'ON a.id_nama_data = b.id left join produk c ON a.id_produk=c.id left join item d ' +
                 'ON a.id_item = d.id left join negara e ON a.id_negara = e.id left join provinsi f ' +
-                'ON a.id_provinsi = f.id left join kota g ON a.id_kota = g.id WHERE a.id='
-    // var query = 'SELECT id, id_nama_data, data_x, data_y from data WHERE id='
+                'ON a.id_provinsi = f.id left join kota g ON a.id_kota = g.id WHERE b.nama = '
+    
 
-    connection.query(query+id, function (error, results, fields) {
+    connection.query(query+nama_data4, function (error, results, fields) {
     
 		if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  	}else{
             
-            try {   
-                df = Object.values(results[0])
-
+            try {
+                var tampung_sementara = []
+                for(var aa = 0; aa < results.length; aa++){   
+                
+                df = Object.values(results[aa])
                 data_x1 = df[2].split(",")
                 var tampung_x = []
                 for(var i = 0; i < data_x1.length; i++){tampung_x.push(data_x1[i])}
@@ -59,82 +66,79 @@ router.get('/:id', function(req, res, next) {
                 var tampung_satuan = []
                 for(var s = 0; s < data_x1.length; s++){tampung_satuan.push(df[9])}
 
+
                 var tampung_sumber = []
-                for(var t = 0; t < data_x1.length; t++){tampung_sumber.push(df[10])}
-
-
-                const lf = new DataFrame({
-                    column1: tampung_id, // <------ A column
-                    column2: tampung_nama_data,
-                    column3: tampung_x,
-                    column4: tampung_y,
-                    column5: tampung_nama_produk,
-                    column6: tampung_item,
-                    column7: tampung_negara,
-                    column8: tampung_provinsi,
-                    column9: tampung_kota,
-                    column10: tampung_satuan,
-                    column11: tampung_sumber
-
-                }, ['Id', 'Nama Data', 'Data X', 'Data Y', 'Nama Produk', 'Item', 'Negara', 'Provinsi', 'Kota', 'satuan', 'Sumber']);
-
-                // ff = lf.restructure(['No', 'Nama Data', 'Data x', 'Data y', 'Nama Produk'])
-
-                lf.toCSV(true, 'databoks.csv');
-                res.download('databoks.csv')
-                
+                var hasil_split = df[10].split(",")
+                for(var t = 0; t < data_x1.length; t++){tampung_sumber.push(hasil_split[0])}
                 
 
+                var lf = new dataForge.DataFrame({
+                    columns: {
+                        Nama_Data: tampung_nama_data,
+                        Data_X: tampung_x,
+                        Data_Y: tampung_y,
+                        Nama_Produk: tampung_nama_produk,
+                        Item: tampung_item,
+                        Negara: tampung_negara,
+                        Provinsi: tampung_provinsi,
+                        Kota: tampung_kota,
+                        Satuan: tampung_satuan,
+                        Sumber: tampung_sumber,
+                    },
+                });
 
-                // var fields = ["id", "id_nama_data", "data_x", "data_y"];
-                // const opts = { fields };
-                // const hasilcsv = json2csv(results, opts);
-                // console.log(hasilcsv);
-                
-                // fs.writeFile('databoks.csv', hasilcsv, function(err) {
-                //     res.download('databoks.csv')
-                // }) 
+                // var lf = new DataFrame({
+                //     column1: tampung_nama_data,
+                //     column2: tampung_x,
+                //     column3: tampung_y,
+                //     column4: tampung_nama_produk,
+                //     column5: tampung_item,
+                //     column6: tampung_negara,
+                //     column7: tampung_provinsi,
+                //     column8: tampung_kota,
+                //     column9: tampung_satuan,
+                //     column10: tampung_sumber
+
+
+                //    }, ['Nama Data', 'Data X', 'Data Y', 'Nama Produk', 'Item', 'Negara', 'Provinsi', 'Kota', 'satuan', 'Sumber']);
+
+                tampung_sementara.push(lf)
+                }
+
+                var df0 = new dataForge.DataFrame({
+                    columns: {
+                        Nama_Data: [],
+                        Data_X: [],
+                        Data_Y: [],
+                        Nama_Produk: [],
+                        Item: [],
+                        Negara: [],
+                        Provinsi: [],
+                        Kota: [],
+                        Satuan: [],
+                        Sumber: [],
+                    },
+                });
+
+
+                var haha = df0.concat(tampung_sementara)
+                var lala = haha.toCSV()
+                // var lala = haha.toCSV(true, 'databoks.csv')
+
+                fs.writeFile('databoks.csv', lala, function(err) {
+                    if (err) throw err;
+                     res.download('databoks.csv');
+                  });
 
             } 
             
             catch (err) {
                 console.error(err);
-            }       
+            } 
+            
         }
       });  
 });
 
-// console.log(results)
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-// try {            
-//     const opts = {};
-//     const hasilcsv = json2csv(results);
-//     fs.writeFile('file.csv', hasilcsv, function(err) { 
-//     })
-//   } catch (err) {
-//     console.error(err);
-//   }
-//   res.download('/home/fakhri/Downloads/file.csv');
-
-
-// var fields = ["id", "id_nama_data", "data_x", "data_y"];
-//             const opts = { fields };
-//             const hasilcsv = json2csv(results, opts);
-//             console.log(hasilcsv);
-            
-//             fs.writeFile('file.csv', hasilcsv, function(err) {
-//               if (err) throw err;
-//               console.log("file saved")})   
-            
-//               res.download('file.csv') 
--- users.js
+++ users.js
@@ -3,16 +3,34 @@ var router = express.Router();
 // let url ="http://localhost:3000/download/";
 // let url ="http://192.168.100.5:3000/download/";; Rumah
 // let url ="http://192.168.88.135:3000/download/"; Kantor
-// let url ="http://192.168.1.7:3000/download/"; Kosan
-let url ="http://open.katadata.co.id:3000/download/";
-// let url ="http://192.168.88.135:3000/download/"; 
+// var url ="http://192.168.1.8:3000/download/"; Kosan
+let url ="http://open.katadata.co.id:3000/download/"
+// let url ="http://open.katadata.co.id:3000/download/"; Server
+
+// var url ="http://192.168.88.135:3000/download/"; 
 
 
 router.get('/:awal', function(req, res, next) {
 	var awalx = req.params.awal;
-	connection.query('select data.id, nama_data.nama as nama_data, ' +
-	                 'perusahaan.nama as instansi, data.description as deskripsi, data.sumber as sumber, data.date_created as date_created,data.date_modified as date_modified, users.username as nama_pengunggah, users.email as email from data, nama_data, perusahaan, users where nama_data.id = data.id_nama_data and perusahaan.id = data.instansi and users.id = data.user_created order by data.id asc limit '+awalx+", 20", function (error, results, fields)				 
-					 { 
+
+	connection.query('select * from (select data.id, nama_data.nama as nama_data, ' +
+					 'perusahaan.nama as instansi, nama_data.description as deskripsi, ' + 
+					 'data.sumber as sumber, data.date_created as date_created, ' +
+					 'data.date_modified as date_modified, (select username from users ' + 
+					 'where users.id = data.user_created)  as nama_pengunggah, users.email ' +
+					 'as email, (select username from users where users.id = data.user_modified) ' +  
+					 'as nama_pengubah, tabel2.nama_tag as nama_tag , industri.nama as kategori ' +
+					 'from data left join nama_data on nama_data.id = data.id_nama_data left join ' +
+					 'perusahaan on perusahaan.id = data.instansi left join users on users.id ' +
+					 '= data.user_created left join (select tabel.id, GROUP_CONCAT(tabel.nama_tag ' +
+					 'separator '+"','"+') as nama_tag from (select data.id, tagdata.nama as nama_tag ' +
+					 'from data left join rel_data_tagdata on rel_data_tagdata.id_data = ' +
+					 'data.id left join tagdata on tagdata.id = rel_data_tagdata.id_tag) ' +
+					 'as tabel group by tabel.id) as tabel2 on tabel2.id = data.id ' +
+					 'left join industri on industri.id = data.id_industri group by nama_data.nama) as gabungan_nama_data ' +
+					 'order by date_modified desc limit '+awalx+", 20", function (error, results, fields)				 
+														
+	{ 
 	
 		if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
@@ -20,7 +38,7 @@ router.get('/:awal', function(req, res, next) {
 
 			var link = []
             for (a_ in results){
-                link[a_] = url + results[a_].id
+                link[a_] = url + results[a_].nama_data.split(" ").join('-')
 			}
 
 			No = []
@@ -55,17 +73,45 @@ router.get('/:awal', function(req, res, next) {
 
 			var desx = []
             for (h_ in results){
-                desx[h_] = results[h_].deskripsi
+				desx[h_] = results[h_].deskripsi
 			}
 
 			var sumberx = []
             for (i_ in results){
-                sumberx[i_] = results[i_].sumber
+				sumberx_split = results[i_].sumber.split(",")
+                sumberx[i_] = sumberx_split[i_]
 			}
-
+			
 			var emailx = []
             for (j_ in results){
                 emailx[j_] = results[j_].email
+			}
+
+			var nama_pengubahx = []
+            for (l_ in results){
+                nama_pengubahx[l_] = results[l_].nama_pengubah
+			}
+
+
+			var tag = []
+            for (m_ in results){
+                tag[m_] = results[m_].nama_tag
+			}
+
+			var kategori = []
+            for (n_ in results){
+                kategori[n_] = results[n_].kategori
+			}
+
+
+			/////////////////////////////////////////////////////////////////////////
+			//Cleaning
+
+			var desx2 = []
+            for (k_ in desx){
+				var ubah =  desx[k_]
+				desx2[k_] = ubah.replace("<p>", "");
+				desx2[k_] = desx2[k_].replace("</p>", "");
 			}
 
 			kumpulan = {}
@@ -78,19 +124,29 @@ router.get('/:awal', function(req, res, next) {
 				kumpulan[No[z_]]["Date_modified"] = String(date_modifiedx[z_])
 				kumpulan[No[z_]]["Nama Pengunggah"] = String(nama_pengunggahx[z_])
 				kumpulan[No[z_]]["Email Pengunggah"] = String(emailx[z_])
+				kumpulan[No[z_]]["Nama Pengubah"] = String(nama_pengubahx[z_])
 				kumpulan[No[z_]]["Sumber"] = String(sumberx[z_])
-				kumpulan[No[z_]]["Deskripsi"] = String(desx[z_])
+				kumpulan[No[z_]]["Deskripsi"] = String(desx2[z_])
+				kumpulan[No[z_]]["Kategori"] = String(kategori[z_])
+
+				try{
+					var tagx = tag[z_].split(",")
+					kumpulan[No[z_]]["Nama Tag"] = {}
+					for(var i = 0; i < tagx.length; i++){					
+						kumpulan[No[z_]]["Nama Tag"][String(i+1)] = String(tagx[i])
+					}
+				}
+				catch(err){}
+
 
 			}
 
 
-			lanjut = "http://open.katadata.co.id:3000/users/"+String(parseInt(awalx)+20)
+			lanjut = "http://192.168.88.135:3000/users/"+String(parseInt(awalx)+20)
 
 			res.send(JSON.stringify({"Response": kumpulan, "Next": lanjut}));
-			// res.send(JSON.stringify({"Response": kumpulan, "Next": lanjut}));
 
-
-	  	}
+		}		
   	});
 });
 
